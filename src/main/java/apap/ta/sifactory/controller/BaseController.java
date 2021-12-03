6e4d2663a5ccf5fb2535d2e031228de1fc72b080
aishas <aishashashas@gmail.com>
2021-12-03 22:30:01 +0700

    feat: done security config

--- src/main/java/apap/ta/sifactory/controller/BaseController.java
+++ src/main/java/apap/ta/sifactory/controller/BaseController.java
@@ -9,33 +9,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BaseController {
 
+    @RequestMapping("/")
+    private String home() {
+        return "home";
+    }
+
     @RequestMapping("/login")
     public String login(){
         return "login";
     }
 
-    @GetMapping("/factorymanager")
-    public String homeFactoryManager(Model model) {
-        return "home-factory-manager";
-    }
-
-    @GetMapping("/admin")
-    public String homeAdmin(Model model) {
-        return "home-admin";
-    }
-
-    @GetMapping("/gudang")
-    public String homeStaffGudang(Model model) {
-        return "home-staff-gudang";
-    }
-
-    @GetMapping("/operasional")
-    public String homeStaffOperasional(Model model) {
-        return"home-staff-operasional";
-    }
-
-    @GetMapping("/kurir")
-    public String homeStaffKurir(Model model) {
-        return"home-staff-kurir";
-    }
+//    @GetMapping("/factorymanager")
+//    public String homeFactoryManager(Model model) {
+//        return "home-factory-manager";
+//    }
+//
+//    @GetMapping("/admin")
+//    public String homeAdmin(Model model) {
+//        return "home";
+//    }
+//
+//    @GetMapping("/gudang")
+//    public String homeStaffGudang(Model model) {
+//        return "home-staff-gudang";
+//    }
+//
+//    @GetMapping("/operasional")
+//    public String homeStaffOperasional(Model model) {
+//        return"home-staff-operasional";
+//    }
+//
+//    @GetMapping("/kurir")
+//    public String homeStaffKurir(Model model) {
+//        return"home-staff-kurir";
+//    }
 }
