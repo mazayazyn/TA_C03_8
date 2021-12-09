12ca90f1533824fe6d7dfbb5d4ebb9d54451a534
Herbiyona <90308503+herbiyona@users.noreply.github.com>
2021-12-10 02:15:59 +0700

    feat: fitur 4 fix param from sibusiness (#38)
    * code: update model
    
    * code: feature 16 add pegawai first commit
    
    * code: login logout
    
    * feat: drop git ignore file
    
    * feat: fixed add pegawai
    
    * feat: fixed add pegawai
    
    * feat: fixed add pegawai
    
    * feat: add code get item to siitem
    
    * feat : fitur 4 done
    
    * feat : fitur 4 done
    
    * feat: tambah handle counter fitur 4
    
    * feat : fitur 17 done
    
    * buat heroku
    
    * buat heroku2
    
    * buat heroku3
    
    * buat heroku4
    
    * buat heroku5
    
    * buat heroku6
    
    * coba heroku
    
    * bismillah heroku bisa
    
    * coba heroku bismillah
    
    * bismillah heroku bisa 2
    
    * bismillah heroku final
    
    * feat: fitur 13 done
    
    * feat: fitur 4 fix param from sibusiness

--- src/main/java/apap/ta/sifactory/rest/Setting.java
+++ src/main/java/apap/ta/sifactory/rest/Setting.java
@@ -2,5 +2,5 @@ package apap.ta.sifactory.rest;
 
 public class Setting {
     final public static String siItemUrl = "https://si-item.herokuapp.com/api/item";
-    final public static String proposeUrl = "http://0dca-149-110-97-253.ngrok.io";
+    final public static String siBusinessUrl = "https://sibusiness-c03-7.herokuapp.com";
 }
