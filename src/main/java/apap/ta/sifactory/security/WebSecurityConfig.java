af9eee580edd5b8218c315d17451a76c7511c0b5
Herbiyona <hryjkt@gmail.com>
2021-12-09 08:51:54 +0700

    buat heroku

--- src/main/java/apap/ta/sifactory/security/WebSecurityConfig.java
+++ src/main/java/apap/ta/sifactory/security/WebSecurityConfig.java
@@ -68,10 +68,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
-    @Autowired
-    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
-    }
+//    @Autowired
+//    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+//    }
 
 //    @Autowired
 //    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
