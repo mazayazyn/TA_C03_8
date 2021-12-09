3fe037967177be6806e23a19ec587100a6326510
Herbiyona <90308503+herbiyona@users.noreply.github.com>
2021-12-09 14:06:09 +0700

    bismillah heroku lagi :D (#36)
    * code: update model
    
    * code: feature 16 add pegawai first commit
    
    * code: login logout
    
    * feat: drop git ignore file
    
    * feat: fixed add pegawai
    
    * feat: fixed add pegawai
    
    * feat: fixed add pegawai
    
    * feat: add code get item to siitem
    
    * feat : fitur 4 done
    
    * feat : fitur 4 done
    
    * feat: tambah handle counter fitur 4
    
    * feat : fitur 17 done
    
    * buat heroku
    
    * buat heroku2
    
    * buat heroku3
    
    * buat heroku4
    
    * buat heroku5
    
    * buat heroku6
    
    * coba heroku
    
    * bismillah heroku bisa
    
    * coba heroku bismillah
    
    * bismillah heroku bisa 2
    
    * bismillah heroku final

--- src/main/java/apap/ta/sifactory/repository/MesinDB.java
+++ src/main/java/apap/ta/sifactory/repository/MesinDB.java
@@ -3,9 +3,12 @@ package apap.ta.sifactory.repository;
 import apap.ta.sifactory.model.MesinModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface MesinDB extends JpaRepository<MesinModel, Integer>{
     Optional<MesinModel> findByIdMesin(Integer idMesin);
 }
