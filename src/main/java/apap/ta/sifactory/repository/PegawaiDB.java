a52842297c6af3d2cbdd95bc9250a40dcfe84bc4
Herbiyona <hryjkt@gmail.com>
2021-12-09 14:07:07 +0700

    bismillah heroku final

--- src/main/java/apap/ta/sifactory/repository/PegawaiDB.java
+++ src/main/java/apap/ta/sifactory/repository/PegawaiDB.java
@@ -3,9 +3,12 @@ package apap.ta.sifactory.repository;
 import apap.ta.sifactory.model.PegawaiModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
+@Transactional
 public interface PegawaiDB extends JpaRepository<PegawaiModel, Integer>{
     PegawaiModel findByUsername(String username);
 }
--- src/main/java/apap/ta/sifactory/repository/RoleDB.java
+++ src/main/java/apap/ta/sifactory/repository/RoleDB.java
@@ -4,7 +4,10 @@ import apap.ta.sifactory.model.RoleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
+@Transactional
 public interface RoleDB extends JpaRepository<RoleModel, Long>{
 
 }
