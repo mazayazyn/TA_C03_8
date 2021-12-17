c8c2376d249e98e88248c19d4a7a2b0819a2db9f
indonesia mazaya <indonesiamazaya@gmail.com>
2021-12-17 14:07:45 +0700

    bismillah

--- src/main/java/apap/ta/sifactory/controller/ItemController.java
+++ src/main/java/apap/ta/sifactory/controller/ItemController.java
@@ -84,13 +84,11 @@ public class ItemController {
     ){
         ItemDetail item = itemRestService.getItemByUUID(uuid);
         model.addAttribute("item", item);
-//        model.addAttribute("detailProduksi", produksiService.getProduksiByItem(uuid));
-//        if (produksiService.getProduksiByItem(uuid) != null) {
+        model.addAttribute("detailProduksi", produksiService.getProduksiByItem(uuid));
 //            model.addAttribute("namaPegawai",  produksiService.getProduksiByItem(uuid).getPegawai().getNama());
 //            model.addAttribute("namaMesin",  produksiService.getProduksiByItem(uuid).getMesin().getNama());
-//            return "detail-item-with-produksi";
 //        }
-        return "detail-item-no-produksi";
+        return "detail-item";
     }
 
     //Fitur 7
