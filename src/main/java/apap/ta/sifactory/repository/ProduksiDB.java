0d181ca8835ec1ef13204be107b65ca249578772
mazayazyn <86276745+mazayazyn@users.noreply.github.com>
2021-12-23 17:42:44 +0700

    feat: change repository method findAllByIdItem (#85)

--- src/main/java/apap/ta/sifactory/repository/ProduksiDB.java
+++ src/main/java/apap/ta/sifactory/repository/ProduksiDB.java
@@ -5,11 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 @Transactional
 public interface ProduksiDB extends JpaRepository<ProduksiModel, Integer>{
-    Optional<ProduksiModel> findByIdItem(String req);
-    ProduksiModel findByIdProduksi(Integer idProduksi);
+    List<ProduksiModel> findAllByIdItem(String req);
 }
