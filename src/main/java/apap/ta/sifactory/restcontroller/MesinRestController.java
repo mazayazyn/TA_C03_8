81d5f585016cd4eafd3a62ccd1df09b19c425c18
skyhaf <hafiznasution2002@gmail.com>
2021-12-24 20:59:03 +0700

    add documentation section

--- src/main/java/apap/ta/sifactory/restcontroller/MesinRestController.java
+++ src/main/java/apap/ta/sifactory/restcontroller/MesinRestController.java
@@ -5,12 +5,9 @@ import apap.ta.sifactory.rest.BaseResponse;
 import apap.ta.sifactory.rest.MesinDetail;
 import apap.ta.sifactory.service.MesinService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 
@@ -19,17 +16,6 @@ import java.util.*;
 public class MesinRestController {
     @Autowired
     private MesinService mesinService;
-
-    @GetMapping(value = "/mesin/{idMesin}")
-    private MesinModel retrieveMesin(@PathVariable("idMesin") Integer idMesin) {
-        try {
-            return mesinService.getMesinByIdMesin(idMesin);
-        } catch (NoSuchElementException e) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "Id Mesin " + String.valueOf(idMesin) + " Not Found,"
-            );
-        }
-    }
 
     @GetMapping(value = "/list-mesin")
     private BaseResponse<List<MesinDetail>> getListMesin() {
